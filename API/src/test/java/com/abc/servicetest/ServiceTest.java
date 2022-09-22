02c72c65676a2346786499de8a8f80b1d6c70a39
yogeshseroke <serokeyogesh7744@gmail.com>
2022-09-22 19:07:11 +0530

    testcases for service class

--- API/src/test/java/com/abc/servicetest/ServiceTest.java
+++ API/src/test/java/com/abc/servicetest/ServiceTest.java
@@ -2,25 +2,22 @@ package com.abc.servicetest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import com.abc.model.Student;
 import com.abc.repository.StudentRepository;
 import com.abc.service.StudentService;
 
 @SpringBootTest
 public class ServiceTest {
-	private static final int Student = 0;
+	
 	Student student2;
 	List<Student> list;
 	Integer studentId;
@@ -44,9 +41,7 @@ public class ServiceTest {
 		student2.setStudentId(12);
 		list.add(student1);
 		list.add(student2);
-
 	}
-
 	@Test
 	public void saveStudent_test() {
 		Student student1 = new Student();
@@ -56,19 +51,15 @@ public class ServiceTest {
 		student2 = studentService.saveStudent(student1);
 		assertEquals(student1.toString(), student2.toString());
 	}
-
 	@Test
 	public void getStudent_test() {
 		Student student1 = new Student();
 		student1.setStudentName("abhishak");
 		student1.setStudentId(12);
-
 		when(repo.findAll()).thenReturn(list);
 		List<Student> studentList = studentService.getStudent();
 		assertEquals(list, studentList);
-
 	}
-
 	@Test
 	public void updateStudent_test() {
 		Student student = new Student();
@@ -77,31 +68,22 @@ public class ServiceTest {
 		when(repo.findById(student.getStudentId())).thenReturn(Optional.of(student));
 		student.setStudentId(13);
 		student.setStudentName("Ram");
-
 		when(repo.save(Mockito.any(Student.class))).thenReturn(student);
 		Student updatedStudent = studentService.updateStudent(student, studentId);
-
 		assertEquals(student.toString(), updatedStudent.toString());
-
 	}
-
 	@Test
 	void deletStudent_Test() {
-
 		Student student1 = new Student();
 		student1.setStudentName("abhishak");
 		student1.setStudentId(12);
 		student2 = studentService.saveStudent(student1);
 		assertEquals(student1.toString(), student2);
-
 	}
-
 	private Student getStudent() {
 		Student student = new Student();
 		student.setStudentName("abhishak");
 		student.setStudentId(12);
-
 		return student;
 	}
-
 }
